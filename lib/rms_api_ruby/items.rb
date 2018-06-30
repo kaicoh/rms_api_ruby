f968d5655e03e6d45693456d3051cbf887b6b5e7
Kaicoh <sumireminami@gmail.com>
2018-06-30 18:11:11 +0900

    Implement items api

--- lib/rms_api_ruby/items.rb
+++ lib/rms_api_ruby/items.rb
@@ -1,6 +1,43 @@
-require 'rms_api_ruby/chain/http_client'
+require 'rms_api_ruby/item/base'
 
 module RmsApiRuby
   class Items
+    class << self
+      def update(args)
+        Flow.new.
+          chain(response: :response) { Client.new(args).call }.
+          on_dam { |error| handle_error(error) }.
+          outflow.
+          try(:response)
+      end
+
+      private
+
+      def handle_error(error)
+        raise error
+      rescue ServerError, AuthenticationError => e
+        RmsApiRuby.configuration.logger.error(e.message)
+      end
+    end
+
+    class Client < RmsApiRuby::Item::Base
+      private
+
+      def http_method
+        :post
+      end
+
+      def url
+        "https://api.rms.rakuten.co.jp/es/#{api_version}/items/update"
+      end
+
+      def start_message
+        "RMS ItemsAPI 'UPDATE' started. args: #{@args.inspect}"
+      end
+
+      def complete_message
+        "RMS ItemsAPI 'UPDATE' completed."
+      end
+    end
   end
 end
--- spec/integration/items_spec.rb
+++ spec/integration/items_spec.rb
@@ -1,11 +1,54 @@
 require 'spec_helper'
 require 'hashie/mash'
-
 require 'support/shared_contexts/config'
 require 'support/shared_contexts/logger'
+require 'support/shared_contexts/webmock'
 
 RSpec.describe RmsApiRuby::Items do
+  include_context 'shared config'
+  include_context 'shared logger'
+  include_context 'use webmock'
+
+  let(:url) { 'https://api.rms.rakuten.co.jp/es/1.0/items/update' }
+
   describe '::update' do
-    it 'updates the multiple items'
+    subject { described_class.update(args) }
+    let(:args) { { item_url: 'myItem01' } }
+    let(:response) do
+      {
+        status: 'N00',
+        itemsUpdateResult: {
+          item: 'item001'
+        }
+      }
+    end
+    let!(:stub_req) do
+      stub_request(:post, url).to_return(
+        status: 200,
+        body: response.to_xml(root: :result)
+      )
+    end
+
+    it 'returns a Hashie Mash instance' do
+      expect(subject).to be_an_instance_of Hashie::Mash
+    end
+
+    it 'returns correct output' do
+      response = subject
+      expect(response.status).to eq 'N00'
+      expect(response.items_update_result.item).to eq 'item001'
+    end
+
+    it 'logs the start message' do
+      expect(logger_mock).to receive(:info).
+        with("RMS ItemsAPI 'UPDATE' started. args: {:item_url=>\"myItem01\"}")
+      subject
+    end
+
+    it 'logs the complete message' do
+      expect(logger_mock).to receive(:info).
+        with("RMS ItemsAPI 'UPDATE' completed.")
+      subject
+    end
   end
 end
