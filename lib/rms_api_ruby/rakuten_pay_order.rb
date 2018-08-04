8f0051478afcee24a0ba9c7a58d46d75771efb0a
Kaicoh <sumireminami@gmail.com>
2018-08-04 16:28:21 +0900

    Implement RakutenPayOrderAPI

---
+++ lib/rms_api_ruby/rakuten_pay_order.rb
@@ -0,0 +1,54 @@
+module RmsApiRuby
+  class RakutenPayOrder
+    BASE_URL = 'https://api.rms.rakuten.co.jp'.freeze
+    API_METHODS = %w[
+      get_order
+      search_order
+      confirm_order
+      update_order_shipping
+      update_order_delivery
+      update_order_orderer
+      update_order_remarks
+      update_order_sender
+      update_order_memo
+      get_payment
+      cancel_order
+      get_sub_status_list
+      update_order_sub_status
+    ].freeze
+
+    class << self
+      API_METHODS.each do |api_method|
+        define_method api_method do |args = nil|
+          conn.post do |req|
+            req.url endpoint(api_method)
+            req.headers['Content-Type'] = 'application/json; charset=utf-8'
+            req.headers['Authorization'] = RmsApiRuby::Authentication.key
+            req.body = args
+          end
+        end
+      end
+
+      private
+
+      def conn
+        Faraday.new(url: BASE_URL) do |faraday|
+          faraday.request  :url_encoded
+          faraday.request  :json
+
+          faraday.response :logger
+          faraday.response :xml,  content_type: /\bxml$/
+          faraday.response :json, content_type: /\bjson$/
+
+          faraday.use :instrumentation
+          faraday.adapter Faraday.default_adapter
+        end
+      end
+
+      def endpoint(api_method)
+        "/es/#{RmsApiRuby.configuration.rakuten_pay_order_api_version}" \
+        "/order/#{api_method.to_s.camelize(:lower)}/"
+      end
+    end
+  end
+end
--- rms_api_ruby.gemspec
+++ rms_api_ruby.gemspec
@@ -38,6 +38,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'faraday'
   spec.add_dependency 'faraday_middleware'
   spec.add_dependency 'hashie'
+  spec.add_dependency 'multi_xml'
   spec.add_dependency 'savon', '~> 2.12.0'
   spec.add_dependency 'waterfall', '~> 1.2.0'
 
